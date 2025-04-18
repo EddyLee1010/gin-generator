ccc32ed507e31037cfb4ec25de347ff4ffdf952e
liziqiang_MacBookPro <120054642@qq.com>
2025-04-19 00:26:06 +0800

    修改config模版

--- README.md
+++ README.md
@@ -18,5 +18,10 @@ go install github.com/eddylee1010/gin-generator@latest
 #### 使用说明
 
 1. gin-generator -h 获取帮助
-2. gin-generator init 生成本工具的配置文件
-3. 未完待续
+2. gin-generator version 查看当前版本号
+3. gin-generator gen 所有生成命令的父级命令
+   * gin-generator gen project 生成项目结构
+   * gin-generator gen model 创建数据库模型文件
+   * gin-generator gen service 创建service文件
+   * gin-generator gen controller 创建controller文件
+   * gin-generator gen router 创建router文件
--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ var rootCmd = &cobra.Command{
 	Use:   "gin-generator",
 	Short: "🕹️一款自动创建基于gin+GORM项目的cli小工具😂",
 	Long: `💡 gin-generator🐔 可以快速把你建立起一个gin+GORM的项目
-💡 包括自动生成数据库模型、service+DTO、controller、router的代码`,
+💡 包括自动生成项目结构、数据库模型+query、service+DTO、controller、router的代码`,
 }
 
 var genCmd = &cobra.Command{
@@ -18,8 +18,17 @@ var genCmd = &cobra.Command{
 	Short: "生成命令 🔑help 获取使用方法",
 }
 
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "获取当前版本号",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v0.1.0")
+	},
+}
+
 func init() {
-	rootCmd.AddCommand(genCmd)
+	rootCmd.AddCommand(genCmd) // 添加gen子命令
+	rootCmd.AddCommand(versionCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
